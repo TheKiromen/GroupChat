3fa27522f32e235da9149d8e01bf6c80dbfcb54d
TheKiromen <TheKiromen@gmail.com>
2020-04-04 16:14:22 +0200

    Cleaning up

--- Client/src/MainApp.java
+++ Client/src/MainApp.java
@@ -5,33 +5,12 @@ import java.net.Socket;
 
 public class MainApp {
 
-    private Socket connection;
-    private ObjectOutputStream outToSever;
-
     public static void main(String args[]){
         MainApp app = new MainApp();
         app.run();
     }
 
     private void run(){
-        try {
-            connection = new Socket("127.0.0.1",6789);
-
-            //Send initial message to server
-            outToSever = new ObjectOutputStream(connection.getOutputStream());
-            //TODO Make this send initial message with username
-           // outToSever.writeObject(new Dimension(5,5));
-            outToSever.writeObject(new String("Client's connection message"));
-            outToSever.flush();
-
-            //Start a thread for listening to messages from server
-            Thread t = new Thread(new MessageReceiver(connection));
-            t.start();
-
-        } catch (IOException e) {
-            System.out.println("Error while connecting to server:");
-            System.out.println(e.getMessage());
-        }
 
     }
 }
--- Server/src/MainApp.java
+++ Server/src/MainApp.java
@@ -6,30 +6,12 @@ import java.util.Date;
 
 public class MainApp {
 
-    private ServerSocket server;
-    private Chatroom global = new Chatroom("Global",1);
-    private ArrayList<User> clients = new ArrayList<User>();
-
     public static void main(String[] args){
         MainApp app = new MainApp();
         app.run();
     }
 
     private void run(){
-        try {
-            server = new ServerSocket(6789);
-            String date = (new Date()).toString();
-            System.out.println("Server's sending date: " + date);
-            System.out.println("Server is waiting for client connection...");
-
-        } catch (IOException e) {
-            System.out.println("Error while trying to create server:");
-            System.out.println(e.getMessage());
-        }
-
-        //New Thread to handle connections
-        Thread connections = new Thread(new ConnectionAcceptor(server,global,clients));
-        connections.start();
 
     }
 }
