c7503ec63229b0c5f92fcc527e30a520ab6488dc
TheKiromen <TheKiromen@gmail.com>
2020-05-11 18:50:02 +0200

    Functional chatroom change window

--- Client/src/CreateChatroom.java
+++ Client/src/CreateChatroom.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class CreateChatroom extends JFrame {
     //Component Variables
@@ -14,9 +18,13 @@ public class CreateChatroom extends JFrame {
     private Font componentsFont = new Font("Arial", Font.BOLD, 16);
     private Color fg = new Color(217, 208, 195);
 
+    private ObjectOutputStream outToServer;
+    private Action a = new Action();
+
     //Constructor
-    public CreateChatroom() {
+    public CreateChatroom(ObjectOutputStream o) {
         //Frame setup
+        outToServer=o;
         setUpComponents();
         add(creatingPanel);
         setTitle("Create new chatroom");
@@ -27,6 +35,10 @@ public class CreateChatroom extends JFrame {
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        textField1.addActionListener(a);
+        createButton.addActionListener(a);
+
 
     }
 
@@ -48,5 +60,25 @@ public class CreateChatroom extends JFrame {
         createButton.setForeground(fg);
         createButton.setBackground(Color.getHSBColor(60, 0.12f, 0.22f));
         createButton.setBorder(BorderFactory.createRaisedBevelBorder());
+    }
+
+    private void createUIComponents(){
+        createButton = new MyButton("Create");
+        createButton.setContentAreaFilled(false);
+        createButton.setFocusable(false);
+    }
+
+
+    private class Action implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            try {
+                outToServer.writeObject(new Request(RequestType.CREATE_CHATROOM,textField1.getText()));
+                outToServer.flush();
+                CreateChatroom.this.dispose();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
